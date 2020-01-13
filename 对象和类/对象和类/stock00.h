04b8c49d9003bd86886ac466a172149aed421f66
songxiuyang <867038070@qq.com>
2020-01-13 17:35:12 +0800

    引出类和对象

---
+++ 对象和类/对象和类/stock00.h
@@ -0,0 +1,27 @@
+#ifndef STOCKOO_H_
+#define STOCKOO_H_
+
+#include<string>
+
+class Stock
+{
+private:
+    std::string company;
+    long shares;
+    double total_val;
+    double share_val;
+
+    void set_tot()
+    {
+        total_val = shares * share_val;
+    }
+
+public:
+    void acquire(const std::string & co, long n, double pr);
+    void buy(long num, double price);
+    void sell(long num, double price);
+    void update(double price);
+    void show();
+};
+
+#endif
