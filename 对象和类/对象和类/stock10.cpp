a2a0ba2bb7f9f161938cefd7bc889586c71c37ea
songxiuyang <867038070@qq.com>
2020-01-14 14:08:45 +0800

    构造函数和析构函数

--- 对象和类/对象和类/stock10.cpp
+++ 对象和类/对象和类/stock10.cpp
@@ -1,23 +1,35 @@
 #include<iostream>
-#include "stock00.h"
+#include "stock10.h"
 
-void Stock::acquire(const std::string & co, long n, double pr)
+Stock::Stock()
 {
+    std::cout << "Default constructor called\n";
+    company = "no name";
+    shares = 0;
+    share_val = 0.0;
+    total_val = 0.0;
+}
+
+Stock::Stock(const std::string & co, long n, double pr)
+{
+    std::cout << "Constructor Using " << co << " called\n";
     company = co;
     if(n < 0)
     {
-        std::cout << "Number of shares can't be negative; "
+        std::cerr << "Number of shares can't be negative; "
             << company << " shares set to 0.\n";
-        shares = 0;
     }
     else
     {
         shares = n;
     }
     share_val = pr;
-
-    // total_val
     set_tot();
+}
+
+Stock::~Stock()
+{
+    std::cout << "Bye, " << company << "!\n";
 }
 
 void Stock::buy(long num, double price)
@@ -42,7 +54,7 @@ void Stock::sell(long num, double price)
     if(num < 0)
     {
         cout << "Number of shares sold can't be nagative. "
-                << "Transaction is aborted.\n";
+            << "Transaction is aborted.\n";
     }
     else if(num > shares)
     {
@@ -65,7 +77,7 @@ void Stock::update(double price)
     set_tot();
 }
 
-void Stock::show()
+void Stock::show() const
 {
     using std::cout;
     using std::ios_base;
--- 对象和类/对象和类/stock10.h
+++ 对象和类/对象和类/stock10.h
@@ -1,5 +1,5 @@
-#ifndef STOCKOO_H_
-#define STOCKOO_H_
+#ifndef STOCK10_H_
+#define STOCK10_H_
 
 #include<string>
 
@@ -8,20 +8,24 @@ class Stock
 private:
     std::string company;
     long shares;
-    double total_val;
     double share_val;
-
+    double total_val;
+    
     void set_tot()
     {
         total_val = shares * share_val;
     }
 
 public:
-    void acquire(const std::string & co, long n, double pr);
+    Stock();
+    Stock(const std::string & co, long n = 0, double pr = 0.0);
+    ~Stock();
+
+public:
     void buy(long num, double price);
     void sell(long num, double price);
     void update(double price);
-    void show();
+    void show() const;
 };
 
 #endif
